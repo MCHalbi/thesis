3800388b037a7850a4fcd1a21dff35fbe61b4c1b
Lukas Halbritter <halbritl@informatik.uni-freiburg.de>
2016-12-23 18:56:46 +0100

    Implemented artifacts method.

--- src/filters/filter.cpp
+++ src/filters/filter.cpp
@@ -12,33 +12,77 @@ Filter::Filter() {}
 // _____________________________________________________________________________
 void Filter::bayerArtifacts(const CImg<unsigned char>& input,
   CImg<unsigned char>* output) {
-//  Filter::bayerColor(input, output);
-//
-//  int width = input.width();
-//  int height = input.height();
-//
-//  //            x / b
-//  //       o------------->
-//  //       |
-//  //       |
-//  //       |
-//  // y / a |
-//  //       |
-//  //       |
-//  //       |
-//  //       v
-//  for (int y = 0; y < width; y++) {
-//    for (int x = 0; x < heigth; x++) {
-//      unsigned char red = 0;
-//      unsigned char green = 0;
-//      unsigned char blue = 0;
-//      for (int a = -1; a <= 1; a++) {
-//        for (int b = -1; b <= 1; b++) {
-//
-//        }
-//      }
-//    }
-//  }
+  Filter::bayerColor(input, output);
+
+  int width = input.width();
+  int height = input.height();
+
+  //            x / b
+  //       o------------->
+  //       |
+  //       |
+  //       |
+  // y / a |
+  //       |
+  //       |
+  //       |
+  //       v
+  for (int y = 0; y < width; y++) {
+    for (int x = 0; x < height; x++) {
+      int rMedian = 0;
+      int gMedian = 0;
+      int bMedian = 0;
+      int rCount = 0;
+      int gCount = 0;
+      int bCount = 0;
+      for (int a = -1; a <= 1; a++) {
+        for (int b = -1; b <= 1; b++) {
+          int currentX = x + b;
+          int currentY = y + a;
+          if (currentX >= 0 && currentY >= 0 &&
+              currentX < width && currentY < height) {
+            Color currentFilterColor =
+              getBayerPixelColor(input, currentX, currentY);
+            switch (currentFilterColor) {
+              case RED:
+                rMedian += input(currentX, currentY, 0, 0);
+                rCount++;
+                break;
+              case GREEN:
+                gMedian += input(currentX, currentY, 0, 1);
+                gCount++;
+                break;
+              case BLUE:
+                bMedian += input(currentX, currentY, 0, 2);
+                bCount++;
+                break;
+            }
+          }
+        }
+      }
+      rMedian = rMedian / rCount;
+      gMedian = gMedian / gCount;
+      bMedian = bMedian / bCount;
+      Color filterColor = getBayerPixelColor(input, x, y);
+      switch (filterColor) {
+        case RED:
+          (*output)(x, y, 0, 0) = input(x, y, 0, 0);
+          (*output)(x, y, 0, 1) = (unsigned char) gMedian;
+          (*output)(x, y, 0, 2) = (unsigned char) bMedian;
+          break;
+        case GREEN:
+          (*output)(x, y, 0, 0) = (unsigned char) rMedian;
+          (*output)(x, y, 0, 1) = input(x, y, 0, 1);
+          (*output)(x, y, 0, 2) = (unsigned char) bMedian;
+          break;
+        case BLUE:
+          (*output)(x, y, 0, 0) = (unsigned char) rMedian;
+          (*output)(x, y, 0, 1) = (unsigned char) gMedian;
+          (*output)(x, y, 0, 2) = input(x, y, 0, 2);
+          break;
+      }
+    }
+  }
 }
 
 // _____________________________________________________________________________
--- src/filters/filterMain.cpp
+++ src/filters/filterMain.cpp
@@ -9,23 +9,27 @@ using cimg_library::CImgDisplay;
 int main() {
   const CImg<unsigned char> image("../../image/lena1.png");
   CImg<unsigned char> resultGrayscale(512, 512, 1, 3, 0),
-    resultColor(512, 512, 1, 3, 0);
+    resultColor(512, 512, 1, 3, 0), resultPattern(512, 512, 1, 3, 0);
 
   Filter::bayerGrayscale(image, &resultGrayscale);
   Filter::bayerColor(image, &resultColor);
+  Filter::bayerArtifacts(image, &resultPattern);
 
   CImgDisplay originalDisp(image, "Original"),
     resultGrayscaleDisp(resultGrayscale, "Filtered Image"),
-    resultColorDisp(resultColor, "Filtered Color Image");
+    resultColorDisp(resultColor, "Filtered Color Image"),
+    resultPatternDisp(resultPattern, "Image with artifacts");
 
   resultGrayscale.save("./filtered.png");
   resultColor.save("./filteredColor.png");
+  resultPattern.save("./withArtifacts.png");
 
   while (!originalDisp.is_closed() || !resultGrayscaleDisp.is_closed() ||
-    !resultColorDisp.is_closed()) {
+    !resultColorDisp.is_closed() || !resultPatternDisp.is_closed()) {
     originalDisp.wait();
     resultGrayscaleDisp.wait();
     resultColorDisp.wait();
+    resultPatternDisp.wait();
   }
 
   return EXIT_SUCCESS;
